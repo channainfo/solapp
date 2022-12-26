fe7dbdd839850ee2a5503b7bfc4d41b4b1653306
Channa Ly <channa.info@gmail.com>
2022-12-26 23:00:42 +0700

    Add test to Introduction schema

--- __tests__/models/introduction.test.ts
+++ __tests__/models/introduction.test.ts
@@ -2,12 +2,55 @@ import { describe, expect, it } from "@jest/globals";
 import Introduction from "../../models/introduction";
 
 describe('Introduction', () => {
-  it('contructor', () => {
-    let intro = new Introduction('Joe', 'Says hello!')
-    expect(3).toEqual(3)
+  let name = "Joe"
+  let message = "Hello"
+  let subject = new Introduction(name, message)
+  let describedClass = Introduction;
 
-    expect(intro.name).toEqual('Joe')
-    expect(intro.message).toEqual('Says hello!')
+  describe('contructor', () => {
+    it('set attributes correctly', () => {
+      expect(subject.name).toEqual(name)
+      expect(subject.message).toEqual(message)
 
+    })
+  })
+
+  describe('#encode', () => {
+    it("encode the object and return a buffer", () => {
+      let result = subject.encode();
+      expect(result).not.toBeFalsy()
+    })
+  })
+
+  describe(".encode", () => {
+
+    it("encode the subject and return a buffer", () => {
+      let buffer = describedClass.encode(subject);
+      expect(buffer).not.toBe(null)
+    })
+  })
+
+  describe(".decode", () => {
+
+    it("decode the subject and return record if the buffer is valid", () => {
+      let buffer = describedClass.encode(subject);
+      let object = describedClass.decode(buffer)!;
+
+      expect(object.name).toEqual(name)
+      expect(object.message).toEqual(message)
+    })
+
+    it("return empty record if buffer is empty", () => {
+      let buffer = Buffer.alloc(100)
+      let object = describedClass.decode(buffer)!;
+      expect(object.name).toEqual("")
+      expect(object.message).toEqual("")
+    })
+
+    it("return null if the buffer in null", () => {
+      let buffer = undefined
+      let object = describedClass.decode(buffer);
+      expect(object).toBeNull()
+    })
   })
 })
